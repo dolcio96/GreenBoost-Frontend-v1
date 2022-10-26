3207fcd44fe8af4abf456b553ede6383292595c3
Giovanni Misso <giova.misso@gmail.com>
2022-10-27 00:09:01 +0200

    inizio integrazione registrazione

--- components/auth/signup/index.js
+++ components/auth/signup/index.js
@@ -18,9 +18,25 @@ import {
 } from '@chakra-ui/react';
 import { useState } from 'react';
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+import { useForm, Controller } from 'react-hook-form'
 
 const Signup = () => {
   const [showPassword, setShowPassword] = useState(false);
+
+  const {
+    control,
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm()
+
+  async function onSubmit(values) {
+    return await fetch("http://localhost:1234/api/auth/signup", {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(values)
+    })
+  }
 
   return (
     <Flex
@@ -28,7 +44,7 @@ const Signup = () => {
       align={'center'}
       justify={'center'}
       bg={useColorModeValue('gray.50', 'gray.800')}>
-      <Stack spacing={8} mx={'auto'} maxW={'3lg'} py={12} px={6} w={{md:"50%",lg:"45%",'2xl':"30%"}}>
+      <Stack spacing={8} mx={'auto'} maxW={'3lg'} py={12} px={6} w={{ md: "50%", lg: "45%", '2xl': "30%" }}>
         <Stack align={'center'}>
           <Heading fontSize={'4xl'} textAlign={'center'}>
             Sign up
@@ -42,66 +58,73 @@ const Signup = () => {
           bg={useColorModeValue('white', 'gray.700')}
           boxShadow={'lg'}
           p={8}
-          w={{md:"100%"}}>
-           
-          <Stack spacing={4}>   
-              <Box>
-                <FormControl id="firstName" isRequired>
-                  <FormLabel>Company Name</FormLabel>
-                  <Input type="text" />
-                </FormControl>
-              </Box>
-              <Box>
-                <FormControl id="lastName" isRequired>
-                  <FormLabel>TAX Number</FormLabel>
-                  <Input type="text" />
-                </FormControl>
-              </Box>
-            <FormControl id="email" isRequired>
-              <FormLabel>Email address</FormLabel>
-              <Input taype="emil" />
-            </FormControl>
-            <FormControl id="password" isRequired>
-              <FormLabel>Password</FormLabel>
-              <InputGroup>
-                <Input type={showPassword ? 'text' : 'password'} />
-                <InputRightElement h={'full'}>
-                  <Button
-                    variant={'ghost'}
-                    onClick={() =>
-                      setShowPassword((showPassword) => !showPassword)
-                    }>
-                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-                  </Button>
-                </InputRightElement>
-              </InputGroup>
-            </FormControl>
-            <FormControl>
-              <FormLabel>Role</FormLabel>
-              <RadioGroup defaultValue='Seller'>
-                <HStack spacing='24px' justifyContent={"center"}>
-                  <Radio _checked={{ borderColor:"green", borderStyle:"solid",  borderWidth:"5px"}} value='Seller'>Seller</Radio>
-                  <Radio _checked={{ borderColor:"green", borderStyle:"solid", borderWidth:"5px"}}  value='Buyer'>Buyer</Radio>
-                </HStack>
-              </RadioGroup>
-            </FormControl>
-            <Stack spacing={10} pt={2}>
-              <Button
-                loadingText="Submitting"
-                size="lg"
-                bg={'green.400'}
-                color={'white'}
-                _hover={{
-                  bg: 'blue.500',
-                }}>
-                Sign up
-              </Button>
-            </Stack>
-            <Stack pt={6}>
-              <Text align={'center'}>
-                Already a user? <Link color={'green.400'} href={"signin"}>Login</Link>
-              </Text>
-            </Stack>
+          w={{ md: "100%" }}>
+
+          <Stack spacing={4}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <FormControl isRequired>
+                <FormLabel>Company Name</FormLabel>
+                <Input type="text" {...register('companyName')} />
+              </FormControl>
+              <FormControl id="lastName" isRequired>
+                <FormLabel>TAX Number</FormLabel>
+                <Input type="text" {...register('taxNumber')} />
+              </FormControl>
+              <FormControl id="email" isRequired>
+                <FormLabel>Email address</FormLabel>
+                <Input type="text" {...register('email')} />
+              </FormControl>
+              <FormControl id="password" isRequired>
+                <FormLabel>Password</FormLabel>
+                <InputGroup>
+                  <Input type={showPassword ? 'text' : 'password'} {...register('password')} />
+                  <InputRightElement h={'full'}>
+                    <Button
+                      variant={'ghost'}
+                      onClick={() =>
+                        setShowPassword((showPassword) => !showPassword)
+                      }>
+                      {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+              </FormControl>
+              <FormControl>
+                <FormLabel>Role</FormLabel>
+                <Controller
+                  render={({ field: { onChange, value } }) => (
+                    <RadioGroup defaultValue='Seller' name='type' onChange={onChange} value={value}>
+                      <HStack spacing='24px' justifyContent={"center"}>
+                        <Radio _checked={{ borderColor: "green", borderStyle: "solid", borderWidth: "5px" }} value='seller'>Seller</Radio>
+                        <Radio _checked={{ borderColor: "green", borderStyle: "solid", borderWidth: "5px" }} value='buyer'>Buyer</Radio>
+                      </HStack>
+                    </RadioGroup>
+                  )}
+                  name="type"
+                  control={control}
+                />
+
+              </FormControl>
+              <Stack spacing={10} pt={2}>
+                <Button
+                  isLoading={isSubmitting}
+                  type='submit'
+                  loadingText="Submitting"
+                  size="lg"
+                  bg={'green.400'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'blue.500',
+                  }}>
+                  Sign up
+                </Button>
+              </Stack>
+              <Stack pt={6}>
+                <Text align={'center'}>
+                  Already a user? <Link color={'green.400'} href={"signin"}>Login</Link>
+                </Text>
+              </Stack>
+            </form>
           </Stack>
         </Box>
       </Stack>
