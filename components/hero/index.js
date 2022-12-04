ff6eed895eb495d4cc9c523814cbb3fa27bea6d5
Ludovico Dolcini <ludovico.dolcini@gmail.com>
2022-12-04 11:25:06 +0100

    send order

--- components/hero/index.js
+++ components/hero/index.js
@@ -31,6 +31,7 @@ import {
 } from '@chakra-ui/react';
 
 import BackgroudImage from "@public/Images/heroForest2.jpg"
+import Fiume from "@public/Video/fiume.gif"
 
 
 const Hero = () => {
--- components/order/index.js
+++ components/order/index.js
@@ -9,13 +9,13 @@ import {
     useDisclosure,
 } from "@chakra-ui/react";
 
-import PopUp from "@components/modal/message"
+
 
 import PaypalComponet from "@components/paypal"
 import OrderRecap from "@components/order/orderRecap"
 
 const Order = () => {
-const { isOpen, onOpen, onClose } = useDisclosure()
+
 
 
     return (
@@ -36,8 +36,7 @@ const { isOpen, onOpen, onClose } = useDisclosure()
                         </Box>
                     </Flex>
                 </Center>
-                <Button onClick={onOpen}>Open Modal</Button>
-                <PopUp isOpen={isOpen} onOpen={onOpen} onClose={onClose} header={"Thank you for your order!"} text={"Now you can visit your personal area to review the order"} bgColor={"primary"}/>
+
             </Box>
         </>
     )
