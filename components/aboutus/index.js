4a9f0f10d81b26b1763125af1bc5fc55091b89e2
Ludovico Dolcini <ludovico.dolcini@gmail.com>
2023-03-07 21:21:22 +0100

    aboutus

---
+++ components/aboutus/index.js
@@ -0,0 +1,19 @@
+import {
+    Box,
+    Center
+} from '@chakra-ui/react';
+
+
+const AboutUs = () => {
+    return (
+        <>
+            <Box>
+                <Center>
+                    About Us
+                </Center>
+            </Box>
+        </>
+    );
+}
+
+export default AboutUs
--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,15 +1,23 @@
 
 import { ReactElement } from "react"
-import NewNavbar from "@components/test"
+import Layout from "@components/layout"
+import Head from "@components/head"
+import AboutUsComponet from "@components/aboutus"
 
-export default function Aboutus() {
+export default function AboutUs() {
  
     return (
         <>
-         
-            <NewNavbar/>
-
-        </>
+        <Head
+          title="GreenBoost: About Us"
+          description="GreenBoost: About Us"
+        />
+          <AboutUsComponet />
+      </>
     )
 }
 
+AboutUs.getLayout = function getLayout(page) {
+    return <Layout>{page}</Layout>
+  }
+  
