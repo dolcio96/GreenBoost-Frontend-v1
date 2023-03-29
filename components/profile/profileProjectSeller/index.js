3522217d65ed7ce6d42808998d49b6966d673677
Ludovico Dolcini <ludovico.dolcini@gmail.com>
2023-03-29 17:41:23 +0200

    -

--- components/profile/profileProjectSeller/index.js
+++ components/profile/profileProjectSeller/index.js
@@ -1,21 +1,13 @@
 // Chakra imports
 import {
-    Avatar,
-    AvatarGroup,
     Box,
     Button,
     Flex,
     Center,
     Grid,
-    Image,
     Text,
-    useColorMode,
-    useColorModeValue,
     Heading,
     GridItem,
-    VStack,
-    useDisclosure,
-    Spinner,
 } from "@chakra-ui/react";
 
 import { EolicIcon, TrashIcon, BambooIcon, NuclearIcon, ForestIcon, LeafIcon, ChemicalIcon } from "@lib/icons";
@@ -62,9 +54,7 @@ function setChartOptions(projects) {
 
 function ProfileProjectsSeller({ userInfo }) {
     var { data: session, status } = useSession()
-    // console.log("INFO")
-    // console.log(userInfo)
-    // console.log(status)
+
 
     const chartOptions = setChartOptions(userInfo.projects)
 
@@ -74,69 +64,78 @@ function ProfileProjectsSeller({ userInfo }) {
                 <Center>
                     <Heading my={3} color="primary" fontSize={50}>Project Seller</Heading>
                 </Center>
-                <Grid templateColumns={{ base: "1fr", lg: "repeat(3, 1fr)" }} gap='22px' justifyContent={"center"}>
-                    <GridItem colSpan={{ base: 3, lg: 2 }} justifyContent={"center"}>
-                        {
-                            userInfo.projects.map((project, index) => {
-                                project["tablesData"] = {
-                                    type: "Buyers",
-                                    header:
-                                        [
-                                            "Project Name",
-                                            "Buyer",
-                                            "Quantity",
-                                            "value",
-                                            "Date",
-                                            "Blockchain Tx"
-                                        ],
-                                    list: {
-                                        buyer1: ["Project1", "Company1", "10 CC", "40 $", "25/12/2023", "https://mumbai.polygonscan.com/tx/0x99ce5cf9971e860fed4e9236c7e1c1298b630103ebd5e113860315fe45958f3c"],
-                                        buyer2: ["Project2", "Company2", "20 CC", "40 $", "25/12/2023", "https://mumbai.polygonscan.com/tx/0x99ce5cf9971e860fed4e9236c7e1c1298b630103ebd5e113860315fe45958f3c"],
-                                        buyer3: ["Project1", "Company3", "50 CC", "40 $", "25/12/2023", "https://mumbai.polygonscan.com/tx/0x99ce5cf9971e860fed4e9236c7e1c1298b630103ebd5e113860315fe45958f3c"],
+                {status = "loading" ?
+                    <Spinner
+                        thickness='4px'
+                        speed='0.65s'
+                        emptyColor="#b7e4c7"
+                        color="#0B0E3F"
+                        size='xl' /> :
+                    <Grid templateColumns={{ base: "1fr", lg: "repeat(3, 1fr)" }} gap='22px' justifyContent={"center"}>
+                        <GridItem colSpan={{ base: 3, lg: 2 }} justifyContent={"center"}>
+                            {
+                                userInfo.projects.map((project, index) => {
+                                    project["tablesData"] = {
+                                        type: "Buyers",
+                                        header:
+                                            [
+                                                "Project Name",
+                                                "Buyer",
+                                                "Quantity",
+                                                "value",
+                                                "Date",
+                                                "Blockchain Tx"
+                                            ],
+                                        list: {
+                                            buyer1: ["Project1", "Company1", "10 CC", "40 $", "25/12/2023", "https://mumbai.polygonscan.com/tx/0x99ce5cf9971e860fed4e9236c7e1c1298b630103ebd5e113860315fe45958f3c"],
+                                            buyer2: ["Project2", "Company2", "20 CC", "40 $", "25/12/2023", "https://mumbai.polygonscan.com/tx/0x99ce5cf9971e860fed4e9236c7e1c1298b630103ebd5e113860315fe45958f3c"],
+                                            buyer3: ["Project1", "Company3", "50 CC", "40 $", "25/12/2023", "https://mumbai.polygonscan.com/tx/0x99ce5cf9971e860fed4e9236c7e1c1298b630103ebd5e113860315fe45958f3c"],
+                                        }
                                     }
-                                }
-                                return (
-                                    <ProjectRowSellerComponent
-                                        key={index}
-                                        project={project} />
+                                    return (
+                                        <ProjectRowSellerComponent
+                                            key={index}
+                                            project={project} />
 
-                                )
-                            })}
-                    </GridItem>
-                    <GridItem colSpan={{ base: 3, lg: 1 }} justifyContent={"center"}>
-                        <Center>
-                            <Flex direction={"column"}>
-                                <Center>
-                                    <ApexCharts options={chartOptions.options} series={chartOptions.series} type="pie" width={500} />
-                                </Center>
-                                <Center>
-                                    <Button
-                                        w={"90%"}
-                                        color={"black"}
-                                        as={"a"}
-                                        href={"/createproject"}
-                                        p='0px'
-                                        bg='transparent'
-                                        border='1px solid lightgray'
-                                        borderRadius='15px'
-                                        minHeight={{ sm: "200px", md: "200px" }}
-                                        _hover={{
-                                            bg: "primary",
-                                            textColor: "tertiary"
-                                        }}>
-                                        <Flex direction='column' justifyContent='center' align='center'>
-                                            <Co2Icon fontSize="large" />
-                                            <Text fontSize='lg' fontWeight='bold'>
-                                                Other projects? Certify Them through us!
-                                            </Text>
-                                        </Flex>
-                                    </Button>
-                                </Center>
-                            </Flex>
-                        </Center>
-                    </GridItem>
+                                    )
+                                })}
+                        </GridItem>
+                        <GridItem colSpan={{ base: 3, lg: 1 }} justifyContent={"center"}>
+                            <Center>
+                                <Flex direction={"column"}>
+                                    <Center>
+                                        <ApexCharts options={chartOptions.options} series={chartOptions.series} type="pie" width={500} />
+                                    </Center>
+                                    <Center>
+                                        <Button
+                                            w={"90%"}
+                                            color={"black"}
+                                            as={"a"}
+                                            href={"/createproject"}
+                                            p='0px'
+                                            bg='transparent'
+                                            border='1px solid lightgray'
+                                            borderRadius='15px'
+                                            minHeight={{ sm: "200px", md: "200px" }}
+                                            _hover={{
+                                                bg: "primary",
+                                                textColor: "tertiary"
+                                            }}>
+                                            <Flex direction='column' justifyContent='center' align='center'>
+                                                <Co2Icon fontSize="large" />
+                                                <Text fontSize='lg' fontWeight='bold'>
+                                                    Other projects? Certify Them through us!
+                                                </Text>
+                                            </Flex>
+                                        </Button>
+                                    </Center>
+                                </Flex>
+                            </Center>
+                        </GridItem>
 
-                </Grid>
+                    </Grid>
+                }
+
             </Box>
         </>
     );
