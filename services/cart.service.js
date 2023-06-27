f9358c207dc9f006589aee97752ba454fbb80b36
Ludovico Dolcini <ludovico.dolcini@gmail.com>
2023-06-27 22:48:22 +0200

    -

--- services/cart.service.js
+++ services/cart.service.js
@@ -5,5 +5,6 @@ export const getCartServices = {
 };
 
 function getCart(buyerId) {
-   return fetchWrapper.post(process.env.BACKEND_API_URL + `/api/cart/`, buyerId);
+   console.log(buyerId)
+   return fetchWrapper.post(process.env.DATABASE_URL + process.env.DATABASE_PORT + `/api/cart/`, buyerId);
 }
