6a5a17a0c2c8c4eac574d41e67f74724031b0561
Ludovico Dolcini <ludovico.dolcini@gmail.com>
2023-09-07 20:59:23 +0200

    -

--- pages/checkout/index.js
+++ pages/checkout/index.js
@@ -1,21 +1,77 @@
+import React from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
 
-import { ReactElement } from "react"
-
-import Layout from "@components/layout"
 import Head from "@components/head"
-import CheckoutComponet from "@components/checkout"
-export default function ContactUs() {
+import Layout from "@components/layout"
+
+import { useSession, getSession } from 'next-auth/react';
+import CheckoutFormComponent from "@components/checkoutForm";
+import CheckoutComponent from "@components/checkout"
+
+
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+
+export default function Order(props) {
+
+  const { data: session, status } = useSession();
+  const [clientSecret, setClientSecret] = React.useState("");
+
+  const [orderItems, setOrderItem] = React.useState("");
+
+  React.useEffect(async () => {
+    // Create PaymentIntent as soon as the page loads
+    fetch("/api/stripe/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      //body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+      body: JSON.stringify({ items: [props.cart.cart_row] })
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+
+  }, []);
+
+
+  const appearance = {
+    theme: 'stripe',
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
   return (
     <>
       <Head
-        title="GreenBoost: Checkout"
-        description="GreenBoost: Checkout"
+        title="GreenBoost: Order"
+        description="GreenBoost: Order"
       />
-        <CheckoutComponet />
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutComponent cart={props.cart} />
+        </Elements>
+      )}
+
     </>
   )
 }
 
-ContactUs.getLayout = function getLayout(page) {
+Order.getLayout = function getLayout(page) {
   return <Layout>{page}</Layout>
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  const { getCartServices } = require('services');
+
+  const res = await getCartServices.getCart({ "buyer_id": session?.user.id });
+
+  const cart = await res.json()
+  //const userInfo = {"id":"id","name":"name"}
+  return {
+    props: {
+      cart
+    }
+  };
 }
