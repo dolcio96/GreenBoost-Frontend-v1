be016d150f4acac25a064b07020c4bfcef6eaf3a
Ludovico Dolcini <ludovico.dolcini@gmail.com>
2023-12-19 00:47:48 +0100

    -

--- components/home/homeBuySellCreate/card.js
+++ components/home/homeBuySellCreate/card.js
@@ -20,26 +20,6 @@ import {
 
 import NextImage from 'next/image';
 
-const CardComponent = ({ iagepath }) => {
-    let { t } = useTranslation();
-    const MotionBox = motion(Box)
-    const img = require(`../../public/${imagePath}`).default;
-    return (
-        <MotionBox maxW="sm" borderWidth="2px" borderRadius="30px" overflow="hidden" h="500px" borderColor={"#24541A"}
-        initial={{ opacity: 0, scale: 0.5 }} whileInView={{ opacity: 1, scale: 1 }} viewport={{ once: true }} transition={{ duration: 0.3, delay: 0.5, ease: [0, 0.71, 0.2, 1.01] }}>
-
-            <NextImage  w="full" maxH="50%" src={img} alt="Descrizione dell'immagine" />
-            <Flex direction="column" justify="space-between" h="45%">
-                <Box p="6">
-                    <Text mt="2" color="gray.600">
-                    {t('projects.'+info.title.toLowerCase()+'.description')}
-                    </Text>
-                </Box>
-            </Flex>
-        </MotionBox>
-    )
-}
-
 
 
 
