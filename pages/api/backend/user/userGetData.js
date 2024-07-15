6c5a6a6c8214166903153c8b3a38f9f12520b68c
Ludovico Dolcini <ludovico.dolcini@gmail.com>
2024-07-15 21:20:35 +0200

    -

--- pages/api/backend/user/userGetData.js
+++ pages/api/backend/user/userGetData.js
@@ -8,6 +8,7 @@ export default async function handler(req, res) {
     const remoteResponse = await fetchWrapper.get(process.env.BACKEND_API_URL + `/api/user/` + userType + '/' + userID);
     const remoteData = await remoteResponse.json();
     res.status(remoteResponse.status).json(remoteData);
+
   } catch (error) {
     console.error('Errore nell\'inoltro della richiesta:', error);
     res.status(500).json({ message: 'Errore nel server interno' });
